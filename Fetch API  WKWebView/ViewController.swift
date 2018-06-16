c73b8c18249da8a8f7e480d4d29b74f8b5424acd
minikin <djminikin@gmail.com>
2018-06-16 10:41:33 +0200

    Add gitignore

---
+++ Fetch API  WKWebView/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Fetch API  WKWebView
+//
+//  Created by Sasha Prokhorenko on 16.06.18.
+//  Copyright © 2018 Sasha Prokhorenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
